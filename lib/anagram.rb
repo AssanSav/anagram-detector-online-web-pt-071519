cb0d578ed769a6760e9d09810f8af47be0683300
assane savadogo <assane.savadogo81@gmail.com>
2019-08-28 23:15:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,5 @@
 require 'pry'
 
 class Anagram 
-  attr_accessor :word 
-  
-  def initialize(word)
-    @word = word 
-  end 
-  
-  def match(words_array)
-    words_array.select do |word| 
-      @word.split("").sort == word.split("").sort 
-    end
-  end
   
 end
